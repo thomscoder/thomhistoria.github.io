4852c69b401cee6510cf02fadb9dfdf2ffe05aae
thomscoder <moyelysian@icloud.com>
2021-02-26 21:21:41 +0100

    fixed css

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ particlesJS('particles-js',
         }
       },
       "line_linked": {
-        "enable": true,
+        "enable": false,
         "distance": 50,
         "color": "#ffffff",
         "opacity": 0.4,
